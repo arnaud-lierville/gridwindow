05b47b548d459934344221e59b95b509c9b2ea3d
Arnaud Lierville <arnaud.lierville@gmail.com>
2022-06-15 23:09:32 +0200

    alpha

--- js/chiffroscope.js
+++ js/chiffroscope.js
@@ -1,163 +1,35 @@
 /* Paper scene */
 // Global variables
-var nbcolumn = 3
-var columnHeight = paper.view.bounds.height/6  // (fixed in css : 1000px/6)
-var marginNavbar = 80
-var fontSize = 40
-var isNumberHidden = false
-var isResultHidden = true
-
+var marginNavbar = 120
 var gridColor = '#91A8D0'
 var pinkColorCard = '#F0DEE4'
 var pinkStrokeColor = '#D8A9B9'
 var greenColorCard = '#DCECD3'
 var greenStrokeColor = '#88BE69'
+var simpleMultipeColor = '#F64C72'
+var doubleMultipleColor = '#7A80AB'
+var digitColor ='#505050'
 var strokeWidth = 3
+var nbcolumn = 30
+var people = new Group()
+var grid = new Group()
+var isPopulated = true//false
+var isMultipleColor = true//false
+var isResultHidden = true
 
-var cardStack  = {}
-
-var unities = {
-    '-3': 'Millièmes',
-    '-2': 'Centièmes',
-    '-1': 'Dixièmes',
-    '0': 'Unités',
-    '1': 'Dizaines',
-    '2': 'Centaines',
-    '3': 'Milliers',
-    '4': 'Dizaines de milliers',
-    '5': 'Centaines de milliers',
-    '6': 'Millions'
+// Math Global variables
+var n = 10
+var a = Math.floor(Math.random() * 8) + 3
+var b = Math.floor(Math.random() * 8) + 3
+if (a == b) { 
+    if(a != 7) { b = Math.min(10, a + 3) } 
+    else { b = 3 }
 }
 
-var shortCut = {
-    'm*': unities['-3'],
-    'c*': unities['-2'],
-    'd*': unities['-1'],
-    'u': unities['0'],
-    'd': unities['1'],
-    'c': unities['2'],
-    'm': unities['3'],
-    'dm': unities['4'],
-    'cm': unities['5'],
-    'M': unities['6'],
-}
-
-var shortCutToOrder = {
-    'm*': '-3',
-    'c*': '-2',
-    'd*': '-1',
-    'u': '0',
-    'd': '1',
-    'c': '2',
-    'm': '3',
-    'dm': '4',
-    'cm': '5',
-    'M': '6',
-}
-
-/* Utils */
-
-getRandomPosition = function() {
-        //random position
-        var parperSize = paper.view.bounds
-        var x = parperSize.width/2 + (Math.floor(Math.random()*7) - 3)*parperSize.width/(3*(nbcolumn + 1))
-        var y = parperSize.height/3 + (Math.floor(Math.random()*7) - 3)*3*parperSize.height/(columnHeight)
-        return { 'x': x, 'y':y }
-}
-
-function generateCard(isUnity) {
-    var parperSize = paper.view.bounds
-    var randomPosition = getRandomPosition()
-    var columnWitdh = parperSize.width/(nbcolumn +1)
-    if(isUnity) {
-        var order = Math.floor(Math.random()*(7) - 3 ) // unityLevel.value = 3
-        if(unityLevel.value == 2) { order = Math.floor(Math.random() * 7) }
-        if(unityLevel.value == 1) { order = Math.floor(Math.random() * 4) }
-        new Card(randomPosition.x, randomPosition.y, unities[order.toString()], columnWitdh, isUnity, order, isNumberHidden, true)
-    } else {
-        new Card(randomPosition.x, randomPosition.y, Math.floor(Math.random()*(Math.pow(10, level.value))), columnWitdh, isUnity, null, isNumberHidden, true)
-    }
-    displayResult(isResultHidden)
-}
-
-function redrawFromStack(delta) {
-    for(var cardID in cardStack ) {
-        var data = cardStack[cardID]
-        var scale = 1
-        if(delta == 1) { scale = nbcolumn/(nbcolumn + 1) }
-        if(delta == -1) { scale = (nbcolumn + 2)/(nbcolumn + 1) }
-        if(cardStack[cardID]['pos'] <= nbcolumn + 1 ) {
-            new Card(data.x*scale, data.y, data.value, paper.view.bounds.width/(nbcolumn + 1), data.isUnity, data.order, data.cardSide, true)
-        } else {
-            new Card(data.x*scale, data.y, data.value, paper.view.bounds.width/(nbcolumn + 1), data.isUnity, data.order, data.cardSide, false)
-        }
-        delete(cardStack[cardID])
-        displayResult(isResultHidden)
-    }
-}
-
-function computeNumber() {
-    var computedNumber
-    var sumList = []
-    for(var cardID in cardStack ) {
-        var card = cardStack[cardID]
-        if(card.isUnity && card.isOnBoard) {
-            var checksum = card.pos + parseInt(card.order)
-            if(!(sumList.includes(checksum))) { sumList.push(checksum) }
-        }
-    }
-    var correctUnities = sumList.length == 1
-    if(correctUnities) {
-        computedNumber = 0
-        for(var cardID in cardStack) {
-            var card = cardStack[cardID]
-            if(!card.isUnity && card.isOnBoard) {
-                computedNumber += card.value*Math.pow(10, sumList[0] - card.pos)
-            }
-        }
-    }
-    return computedNumber
-}
-
-function displayResult(isResultHidden) {
-    var resultDisplayText = '?'
-    if(!isResultHidden) {
-        resultDisplayText = '!'
-        var computedNumber = Math.round(computeNumber()*100000000)/100000000
-        if(computedNumber) { resultDisplayText = computedNumber.toString().replace('.',',') }
-    }
-    resultDisplay.innerHTML = resultDisplayText
-}
-
-function generateCardFromInput() {
-    var randomPosition = getRandomPosition()
-    var columnWitdh = paper.view.bounds.width/(nbcolumn +1)
-    var currentValue = numberInput.value
-    var generate = false
-    var isUnity = false
-    var order = null
-
-    if(currentValue in shortCut) {
-        order = shortCutToOrder[currentValue]
-        currentValue = shortCut[currentValue]
-        isUnity = true
-        generate = true
-     } else {
-         if(!isNaN(currentValue)) {
-             generate = true
-         }
-     }
-     if(generate && currentValue != '') {
-         new Card(randomPosition.x, randomPosition.y, currentValue, columnWitdh, isUnity, order, isNumberHidden, true)
-         displayResult(isResultHidden)
-        }
-     numberInput.value = ''
-}
-
-function ID() { return Math.random().toString(36).substring(2, 9); }
+var firstValue = Math.floor(Math.random() * 8) + 3
 
 // when view is resized...
-paper.view.onResize = function() { drawApp(paper.view.bounds, nbcolumn, 0) }
+paper.view.onResize = function() { drawApp(isPopulated, isMultipleColor) }
 
 /* Html scene */
 var html =  '<nav class="navbar fixed-bottom navbar-light bg-light">' +
@@ -186,7 +58,7 @@ var html =  '<nav class="navbar fixed-bottom navbar-light bg-light">' +
 
                     '<li class="nav-item">' +
                     '<div class="form-check form-switch custom-switch" data-toggle="tooltip" data-placement="bottom" title="Peupler la grille">' +
-                        '<input id="showNumberSwitch" class="form-check-input" type="checkbox" role="switch" style="transform: scale(1.8);">' +
+                        '<input id="populateSwitch" class="form-check-input" type="checkbox" role="switch" style="transform: scale(1.8);">' +
                         '<label class="form-check-label" style="padding-left: 13px;">Peupler</label>' +
                     '</div>' +
                 '</li>' +
@@ -209,7 +81,7 @@ var html =  '<nav class="navbar fixed-bottom navbar-light bg-light">' +
 
 					'<li class="nav-item">' +
 						'<div class="btn-group">' +
-							'<button class="btn btn-danger" data-toggle="tooltip" data-placement="bottom" title="Nouvelle partie" id="trashButton">' +
+							'<button class="btn btn-danger" data-toggle="tooltip" data-placement="bottom" title="Nouvelle partie" id="redoButton">' +
 								'<i class="fa-solid fa-rotate-right"></i>' +
 							'</button>' +
 							'<button class="btn btn-success" data-toggle="tooltip" data-placement="bottom" title="Aide" id="helpButton">' +
@@ -234,194 +106,140 @@ div.innerHTML = html
 document.body.insertBefore(div, document.body.firstChild);
 
 /* Interaction html <-> canvas */
-var minusButton = document.getElementById('minusButton')
-var plusButton = document.getElementById('plusButton')
-var unityButton = document.getElementById('unityButton')
-var numberButton = document.getElementById('numberButton')
 var numberInput = document.getElementById('numberInput')
+var numberInput2 = document.getElementById('numberInput2')
 var level = document.getElementById('level')
-var showNumberSwitch = document.getElementById('showNumberSwitch')
+var populateSwitch = document.getElementById('populateSwitch')
+var showColorSwitch = document.getElementById('showColorSwitch')
 var showResultSwitch = document.getElementById('showResultSwitch')
-var trashButton = document.getElementById('trashButton')
+var redoButton = document.getElementById('redoButton')
 var helpButton = document.getElementById('helpButton')
 var resultDisplay = document.getElementById('resultDisplay')
 
+// TO KILL
+numberInput.value = a
+numberInput2.value = b
+
 var helpModal = new bootstrap.Modal(document.getElementById('helpModal'), { keyboard: false })
 
-minusButton.onclick = function() {
-    nbcolumn--
-    if(nbcolumn > 1) { 
-        drawApp(paper.view.bounds, nbcolumn, -1)
-    } else {
-        nbcolumn = 2
-    }
-}
-plusButton.onclick = function() {
-    nbcolumn++
-    drawApp(paper.view.bounds, nbcolumn, 1)
-}
-unityButton.onclick = function() { generateCard(true) }
-numberButton.onclick = function() { generateCard(false) }
 numberInput.addEventListener('keyup', function(event) { if(event.key == 'Enter') { generateCardFromInput() } })
-showResultSwitch.addEventListener('change', function() {
-    isResultHidden = !isResultHidden
-    displayResult(isResultHidden)
-})
-showNumberSwitch.addEventListener('change', function() { 
-    if(!isNumberHidden) { numberInput.setAttribute("type", "password") } else { numberInput.setAttribute("type", "text") }
-    isNumberHidden = !isNumberHidden
- })
 
- trashButton.onclick = function() {
-    nbcolumn = 3
-    cardStack  = {}
-    drawApp(paper.view.bounds, nbcolumn, 0)
-    displayResult(isResultHidden)
+showResultSwitch.addEventListener('change', function() {
+    if(!isResultHidden) { 
+        resultDisplay.innerHTML = '?'
+    } else {
+        if(numberInput.value == a && numberInput2.value == b) { 
+            resultDisplay.innerHTML = 'Gagné !'
+        } else {
+            resultDisplay.innerHTML = 'Perdu !'
+        }
+    }
+    isResultHidden = !isResultHidden
+
+})
+showColorSwitch.addEventListener('change', function() {
+    isMultipleColor = !isMultipleColor
+    drawApp(isPopulated, isMultipleColor)
+})
+populateSwitch.addEventListener('change', function() {
+    isPopulated = !isPopulated
+    drawApp(isPopulated, isMultipleColor)
+})
+
+redoButton.onclick = function() { 
+    a = Math.floor(Math.random() * 8) + 3
+    b = Math.floor(Math.random() * 8) + 3
+    if (a == b) { 
+        if(a != 7) { b = Math.min(10, a + 3) } 
+        else { b = 3 }
+    }
+
+    // TO KILL
+    numberInput.value = a
+    numberInput2.value = b
+
+    firstValue = Math.floor(Math.random() * 8) + 3
+    // TO RESTORE
+    // isPopulated = false
+    // populateSwitch.checked = false
+    // isMultipleColor = false
+    // showColorSwitch.checked = false
+    drawApp(isPopulated, isMultipleColor)
 }
 
 helpButton.onclick = function() { helpModal.toggle() }
 
-/* key functions */
-
-function onKeyDown(event) { if (event.key == 'enter') { generateCardFromInput() } }
-
 /* main function */
 
-function drawApp(parperSize, nbcolumn, way) {
+function drawApp(isPopulated, isMultipleColor) {
     project.clear()
+
+    var people = new Group()
     var grid = new Group()
-    var columnWitdh = parperSize.width/(nbcolumn + 1)
-    for(var j=0; j<nbcolumn +1;j++) {
-        var from = new Point(columnWitdh/2 + columnWitdh*j, 0)
-        var to = new Point(columnWitdh/2 + columnWitdh*j, parperSize.height*1.5)
+
+    var squareSize = paper.view.bounds.width/nbcolumn
+    var xShift = (nbcolumn - n)*.5*squareSize
+
+    // grid drawing
+    for(var i = 0; i < n + 1; i++) {
+        var from = new Point(xShift + 0, i*squareSize + marginNavbar)
+        var to = new Point(xShift + n*squareSize, i*squareSize + marginNavbar)
+        var path = new Path.Line(from, to)
+        path.strokeColor = gridColor
+        path.strokeWidth = strokeWidth
+        grid.addChild(path)
+
+        from = new Point(xShift + i*squareSize, 0 + marginNavbar)
+        to = new Point(xShift + i*squareSize, n*squareSize + marginNavbar)
         var path = new Path.Line(from, to)
         path.strokeColor = gridColor
         path.strokeWidth = strokeWidth
         grid.addChild(path)
     }
-    var from = new Point(0, columnHeight + marginNavbar)
-    var to = new Point(parperSize.width, columnHeight + marginNavbar)
-    var path = new Path.Line(from, to)
-    path.strokeColor = gridColor
-    path.strokeWidth = strokeWidth
-    grid.addChild(path)
+
     grid.sendToBack()
     grid.visible = true
-    redrawFromStack(way)
+
+    // hide multiples
+    var value = firstValue
+    for(var i = 0 ; i < n ; i++) {
+        for(var j = 0 ; j < n; j++) {
+
+            if(value%a == 0 || value%b == 0) {
+                var point = new Point(xShift + squareSize/2 + j*squareSize, marginNavbar + squareSize/2 + i*squareSize)
+                var circle = new Path.Circle(point, squareSize/2.2)
+                var currentColor = simpleMultipeColor
+                if (value%a == 0 && value%b == 0 && isMultipleColor) { currentColor = doubleMultipleColor }
+                circle.fillColor = currentColor;
+            }
+            value += 1
+        }
+        value += 10
+    }
+
+    // populate
+    var value = firstValue
+    for(var i = 0 ; i < n ; i++) {
+        for(var j = 0 ; j < n; j++) {
+            if(value%a != 0 & value%b != 0) {
+            var point = new Point(xShift + squareSize/2 + j*squareSize, marginNavbar + squareSize/2 + i*squareSize)
+            var circle = new Path.Circle(point, squareSize/2.2)
+            circle.fillColor = greenColorCard
+            people.addChild(circle)
+            }
+
+            var point = new Point(xShift + squareSize/2 + j*squareSize, marginNavbar + squareSize/1.5 + i*squareSize)
+            var text = new PointText(point)
+            text.justification = 'center'
+            text.fillColor = digitColor
+            text.fontSize = squareSize/2
+            text.content = value
+            people.addChild(text)
+            value += 1
+        }
+        value += 10
+    }
+
+    people.visible = isPopulated
+    people.bringToFront()
 }
-
-/* Card */
-var Card = Base.extend({
-
-    initialize: function(x, y, value, columnWitdh, isUnity, order, cardSide, isOnBoard) {
-
-        var cardWidth = columnWitdh*0.7
-        var cardHeight = columnHeight*0.7
-        
-        this.x = x
-        this.y = y
-        this.value = value
-        this.cardSide = cardSide
-        this.isOnBoard = isOnBoard
-        this.fillColor = pinkColorCard
-        this.strokeColor = pinkStrokeColor
-        this.isUnity = isUnity
-        this.order = order
-        if(isNaN(value)) { 
-            this.fillColor = greenColorCard
-            this.strokeColor = greenStrokeColor
-         }
-        
-        this.path = new Path.Rectangle({
-            topLeft: [x, y],
-               bottomRight: [x + cardWidth, y + cardHeight],
-               radius: 10,
-               strokeWidth: strokeWidth,
-               fillColor: this.fillColor,
-               strokeColor: this.strokeColor
-           })
-
-        this.text = new PointText(new Point(x + cardWidth/2,y + cardHeight/1.6));
-        this.text.justification = 'center';
-        this.text.fillColor = 'black';
-        this.text.fontSize = fontSize
-        this.text.content = value
-        
-        //scaling the text if too long
-        var textWidth = this.text.bounds.width
-        if(cardSide) { this.text.content = '?' }
-        var textNumberOfCharacters = value.toString().length
-        this.scale = 1
-        if(textNumberOfCharacters > cardWidth/(fontSize*0.69)) { this.scale = cardWidth*0.9/textWidth }
-        if(!cardSide) { this.text.scale(this.scale) }
-    
-        this.cardGroup = new Group();
-        this.cardGroup.addChild(this.path)
-        this.cardGroup.addChild(this.text)
-        this.cardGroup.bringToFront()
-
-        this.cardID = ID()
-        cardStack[this.cardID] = {
-            'x': x,
-            'y': y,
-            'value': value,
-            'pos': Math.floor((x - cardWidth/5)/columnWitdh) + 1,
-            'isUnity': this.isUnity,
-            'order': this.order,
-            'cardSide': this.cardSide,
-            'isOnBoard': this.isOnBoard
-        }
-
-        /* methods */
-
-        var that = this
-        var wasMoving = false
-
-        this.cardGroup.onMouseDown = function(event) {
-            that.cardGroup.bringToFront()
-            wasMoving = false
-        }
-
-        this.cardGroup.onMouseUp = function(event) {
-            if(!wasMoving) {
-                if (that.text.content == '?') { 
-                    that.text.content = value
-                    that.text.scale(that.scale)
-                } else { 
-                     that.text.content = '?'
-                     that.text.scale(1/that.scale)
-                     cardStack[that.cardID].cardSide = !that.cardSide
-                    }
-                that.cardSide = !that.cardSide
-                cardStack[that.cardID].cardSide = that.cardSide
-             }
-             that.cardGroup.shadowColor = null;
-             displayResult(isResultHidden)
-        }
-
-        this.cardGroup.onMouseDrag = function(event) { 
-            that.cardGroup.shadowColor = new Color(0, 0, 0);
-            that.cardGroup.shadowBlur = 12;
-            that.cardGroup.shadowOffset = new Point(5, 5);
-            that.cardGroup.bringToFront()
-            
-            that.cardGroup.position.x += event.delta.x;
-            that.cardGroup.position.y += event.delta.y;
-            that.cardGroup.position.y = Math.max(that.cardGroup.position.y, marginNavbar + cardHeight/2)
-
-            cardStack[that.cardID]['x'] = cardStack[that.cardID]['x'] + event.delta.x
-            cardStack[that.cardID]['y'] = Math.max(cardStack[that.cardID]['y'] + event.delta.y, marginNavbar)
-            cardStack[that.cardID]['pos'] = Math.floor((cardStack[that.cardID]['x'] - cardWidth/5)/columnWitdh) + 1
-            wasMoving = true
-            displayResult(isResultHidden)
-        }
-
-        this.cardGroup.onDoubleClick = function(event) {
-            delete(cardStack[that.cardID])
-            console.log(cardStack)
-            that.cardGroup.remove()
-            displayResult(isResultHidden)
-        }
-
-        return this.cardGroup
-    }})
